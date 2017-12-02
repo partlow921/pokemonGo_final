7d6deb19f1c4ba385c4fbda04241a5bc916ece17
partlow921 <33816784+partlow921@users.noreply.github.com>
2017-12-02 16:07:34 -0500

    Update pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -8,8 +8,9 @@ pokedex_db=open("/Users/crowTrobot921/Desktop/PokemonGo_ProjectFiles/pokedex.csv
 #Encoding added to run on Mac
 pokedex_db.readline()
 for line in pokedex_db:
-    pokedex_id, name, type1, type2, weight, height, move_name, move_power, initial_lvl, gen_lvl_cap, evolve_to, evolve_lvl, catch_chance = line.strip().split(",")
-    move=Move(move_name,float(move_power))
+    pokedex_id, name, type1, type2, weight, height, move_name, move_power, move_name2, move_power2, initial_lvl, gen_lvl_cap, evolve_to, evolve_lvl, catch_chance = line.strip().split(",")
+    move=Move(move_name,float(move_power),type1)
+    move2=Move(move_name2,float(move_power2),type2)
     pokemon_info={}
     pokemon_info["pokedex_id"]=int(pokedex_id)
     pokemon_info["name"]=name
@@ -18,13 +19,13 @@ for line in pokedex_db:
     pokemon_info["weight"]=float(weight)
     pokemon_info["height"]=float(height)
     pokemon_info["move"]=move
+    pokemon_info["move2"]=move2
     pokemon_info["initial_lvl"]=int(initial_lvl)
     pokemon_info["gen_lvl_cap"]=int(gen_lvl_cap)
     pokemon_info["evolve_to"]=evolve_to
     pokemon_info["evolve_lvl"]=int(evolve_lvl)
     pokemon_info["catch_chance"]=float(catch_chance)
     #vary catch chance in csv
-    #add multiple move options? different powers?
 
     pokemon_entries[int(pokedex_id)]=pokemon_info
 
