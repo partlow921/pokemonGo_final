db70fa3506a332b3af413e7ad3ba30bb10229551
partlow921 <33816784+partlow921@users.noreply.github.com>
2017-12-02 16:27:04 -0500

    Update pokemonGo_main.py

--- pokemonGo_main.py
+++ pokemonGo_main.py
@@ -5,45 +5,11 @@ import random
 import sqlite3
 import database
 
-
-"""
-Current is for lab assignment and project testing
-Currently only creates new player versus allowing user to log back in
-
-"""
-def login_module():
-    player=Player(input("Please enter your username: "),input("Please enter new password: Your password must:\n\
-                 1. Contain at least 8 letters.\n\
-                 2. Contain at least one uppercase letter, at least one symbol, and at least one digit.\n\
-                 3. The symbol only allows: !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~\n"))
-    print(player,"\n")
-    pokemon1=generate_pokemon_by_name(random.choice(["Charmander","Squirtle","Bulbasaur"]))
-    #does not cap level but selections have level cap between 1-15, so should suffice at player creation.
-    #initial pokemon should actually call capture menu to demonstrate capture function for new player.
-    #Same Pokemon should be generated but would require current capture menu to be modified.
-    player.pokemon_in_hand.append(pokemon1)
-    print("You recieved a level " + str(pokemon1.pokemon_level) + " " + pokemon1.name + " (HP: " + str(pokemon1.hp) + ", CP: " + str(pokemon1.cp) + ")")
-
-    for i in range(10):
-        player.bag.add_item(PokeBall("Poke Ball"))
-        player.bag.add_item(Potion("Potion"))
-        player.bag.add_item(Revive("Revive"))
-    print("10 Potions, Poke Balls and Revives have been added to your bag\n")
-
-    return player
-
-"""
-An additional option for evolutios is added solely for demostration.
-If a pokemon who can't evolve is selected, error will be returned.
-There is no way to handle this error as it is solely for demonstration for the lab and will be removed.
-This can also be demonstrated through battles by would require several encounters to evolve.
-
-"""
 def main_menu(player):
     main_menu="\nWhat do you want to do? Enter a number.\n" +\
                "1: Battle Pokemon\n2: Capture Pokemon\n" +\
-               "3: Use an Item\n4: View Pokemon\n5: Force Evolution\n" +\
-               "6: View Player Stats\n"
+               "3: Use an Item\n4: View Pokemon\n" +\
+               "5: View Player Stats\n"
     print(main_menu)
     option=int(input())
     while(option!=0):
@@ -56,6 +22,8 @@ def main_menu(player):
         elif(option==4):
             player.show_pokemon_in_hand()
         elif(option==5):
+            print(player)
+        elif(option==6):
             print("The following is only for testing evolutions and not actual game mechanics. As a result, selecting a Pokemon who can't evolve and has no evolve_to attribute will return an error.\n")
             poke_list=""
             i=1
@@ -66,12 +34,47 @@ def main_menu(player):
             if(force_evo!=0):
                 poke_evo=player.pokemon_in_hand[force_evo-1]
                 poke_evo.evolve()
-        elif(option==6):
-            print(player)
         else:
             print("Please select a valid option\n")
         option=int(input(main_menu))
+    #database.save_progress(player)
+    #needs tables created to function
     print("Exiting game")
+
+def login_module():
+    player_name=input("Username: ")
+
+    """
+    Below needs tables created to run
+    user_exist=database.check_user_name(player_name)
+    if(user_exist):
+        user_name, user_password, level, experience = database.get_user_info(player_name)
+        while(input("Please enter your password:")!=user_password):
+            print("Incorrect password. Please try again.")
+        player=Player(user_name,user_password,experience,level)
+        database.sync_from_db(player)
+        print("\nWelcome back!\n")
+    else:
+        password=input("Welcome new player! Please create your password:")
+        player=Player(player_name,password,0,1)
+        dabatase.create_new_player(player)
+    """
+    
+
+    player=Player(player_name,input("Please enter new password: "))
+    print(player,"\n")
+    pokemon1=generate_pokemon_by_name(random.choice(["Charmander","Squirtle","Bulbasaur"]))
+    player.pokemon_in_hand.append(pokemon1)
+    print("You recieved a level " + str(pokemon1.pokemon_level) + " " + pokemon1.name + " (HP: " + str(pokemon1.hp) + ", CP: " + str(pokemon1.cp) + ")")
+
+    for i in range(10):
+        player.bag.add_item(PokeBall("Poke Ball"))
+        player.bag.add_item(Potion("Potion"))
+        player.bag.add_item(Revive("Revive"))
+    print("10 Potions, Poke Balls and Revives have been added to your bag\n")
+
+    return player
+
 
 player1=login_module()
 main_menu(player1)
