6e09bd01eb3add5c31d1385c69d405587a0d5e4f
partlow921 <33816784+partlow921@users.noreply.github.com>
2017-12-02 15:56:33 -0500

    Update move.py

--- move.py
+++ move.py
@@ -6,12 +6,11 @@ class Move(object):
     power : (float) pokemon's power
     """
 
-    def __init__(self, name="Default Attack", power=1.0):
+    def __init__(self, name="Default Attack", power=1.0, type="Normal"):
         """Return the name for a specific move and its corresponding power"""
         self.name   = name
         self.power  = power
-        #add attributes for type in order to manage advantages in battle
-        #gauge?
+        self.type   = type
 
     def __str__(self):
         """defines string value for move object when print function used"""
