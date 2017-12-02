c678904c3f32f3a9ac882365e65e23a5916ff8bc
partlow921 <33816784+partlow921@users.noreply.github.com>
2017-12-02 16:08:47 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,5 @@
 from move import *
+from type import *
 from pokedex import pokemon_entries
 import random
 
@@ -19,9 +20,9 @@ class Pokemon(object):
     move.power      : (float) power of move performed parameter of move object
     """
 
-    POKEMON_EXPERIENCE_LEVEL=range(0,9999*1000,1000)
+    POKEMON_EXPERIENCE_LEVEL=range(1000,9999*1000,1000)
 
-    def __init__(self, id_int=0, name='', hp_int=1, cp_int=1, type1='normal', type2='None', move=Move(), weight=0.0, height=0.0, sex='Unknown', catch_chance=1.0, pokemon_level=0, pokemon_xp=0):
+    def __init__(self, id_int=0, name='', hp_int=1, cp_int=1, type1='normal', type2='None', move=Move(), move2=Move(), weight=0.0, height=0.0, sex='Unknown', catch_chance=1.0, pokemon_level=0, pokemon_xp=0):
             if (hp_int<0 or cp_int<0):
                 #additional validations should be added, sef, weight/height
                 print("Values provided were not correct, default Pokemon instance has been created")
@@ -36,10 +37,10 @@ class Pokemon(object):
                 self.height       = 0.0
                 self.sex          = 'Unknown'
                 self.move         = move
+                self.move2        = move2
                 self.catch_chance = 1.0
                 self.pokemon_xp   = 0
                 self.pokemon_level= 0
-                #additional moves
             else:
                 self.pokedex_id   = id_int
                 self.name         = name
@@ -52,26 +53,28 @@ class Pokemon(object):
                 self.height       = height
                 self.sex          = sex
                 self.move         = move
+                self.move2        = move2
                 self.catch_chance = catch_chance
                 self.pokemon_xp   = pokemon_xp 
                 self.pokemon_level= pokemon_level
-                #additional moves
 
     def change_name(self, new_name):
         """Change the name of the pokemon with new_name"""
         self.name   = new_name
 
     def attack(self, move, target_pokemon):
-        #Pokemon types to be compared with multiplier added
-        #Damage needs to be scaled better - either:
-        #further adjustment to move powers (in csv OR dynamic based on level generated, minor increase at level up)
-        #adjust cp/hp so in balance with moves - a pokemon with 5 HP shouldn't do 25 damage unless has type advantage.
-        #restrict generated pokemon even further based on player
-        """The pokemon performs an attack with move and updates hp after attack"""
-        damage_dealt = max(1,int(move.power * (self.cp / target_pokemon.cp)))
+        """
+        Accepts move select from battle menu, with type, to determine multiplier
+        Damage dealt is multiplied based on effects of selected moves type compared to both types of target
+        Adjusts HP and ends if hp has dropped to 0
+        """
+        
+        print(self.name + " used " + str(move.name) + " against "+ target_pokemon.name + "!")
+        
+        type_multiplier = battle_type_eval(move.type, target_pokemon.type1, target_pokemon.type2)
+        damage_dealt = int(max(1,int(move.power * (self.cp / target_pokemon.cp))) * type_multiplier)
         target_pokemon.current_hp -= damage_dealt
 
-        print(self.name + " used " + str(self.move) + " against "+ target_pokemon.name + "!") 
         print(self.name + ' dealt ' + str(damage_dealt) + " damage to " + target_pokemon.name + "!")
 
         if (target_pokemon.current_hp < 0):
@@ -84,6 +87,7 @@ class Pokemon(object):
                      
     def __str__(self):
         """defines string value returned when print function used for pokemon"""
+        
         return str(self.name) +\
                ": \nLevel: "+ str(self.pokemon_level) +" (xp: "+str(self.pokemon_xp)+")"+\
                "\nSex: "+ str(self.sex) +\
@@ -91,11 +95,13 @@ class Pokemon(object):
                "\nHP: "+ str(int(self.current_hp)) +\
                "/" + str(int(self.hp)) +\
                "\nType: "+ str(self.type1)+ ", "+ str(self.type2) +\
-               "\nMove: "+ str(self.move) + " (power: "+ str(self.move.power) +")"\
+               "\nPrimary Move: "+ str(self.move) + " (power: "+ str(self.move.power) +")"\
+               "\nSecondary Move: "+ str(self.move2) + " (power: "+str(self.move2.power)+")"\
                "\nWeight: "+ str(self.weight) + " kg \nHeight: "+ str(self.height)+ " m"
 
     def randomize_status(self):
         """returns random values for pokemon's cp, hp, weight, heigh and sex"""
+        
         self.cp         = random.randint(10*self.pokemon_level,10*(self.pokemon_level*2))
         self.hp         = random.randint(cp//10, 1.5*cp//10)
         self.current_hp = self.hp
@@ -121,16 +127,15 @@ class Pokemon(object):
     def pokemon_increase_experience(self,amount):
         self.pokemon_xp+=int(amount)
         print(self.name + " has gained " + str(amount) + " experience points\n")
-        #Leveling needs to be offset by 1 -- 1000 = Level 2, 2000 = Level 3, etc
-        if(self.pokemon_xp >= self.POKEMON_EXPERIENCE_LEVEL[self.pokemon_level]):
-            while(self.pokemon_xp >= self.POKEMON_EXPERIENCE_LEVEL[self.pokemon_level]):
+        if(self.pokemon_xp >= self.POKEMON_EXPERIENCE_LEVEL[self.pokemon_level-1]):
+            while(self.pokemon_xp >= self.POKEMON_EXPERIENCE_LEVEL[self.pokemon_level-1]):
                 self.pokemon_level_up()
 
 
 class EvolvablePokemon(Pokemon):
 
-    def __init__(self, id_int, name, hp_int, cp_int, type1, type2, move, weight, height, sex, catch_chance, pokemon_level, pokemon_xp, evolve_to_str, evolution_lvl):
-        super(self.__class__, self).__init__(id_int, name, hp_int, cp_int, type1, type2, move, weight, height, sex, catch_chance, pokemon_level, pokemon_xp)
+    def __init__(self, id_int, name, hp_int, cp_int, type1, type2, move, move2, weight, height, sex, catch_chance, pokemon_level, pokemon_xp, evolve_to_str, evolution_lvl):
+        super(self.__class__, self).__init__(id_int, name, hp_int, cp_int, type1, type2, move, move2, weight, height, sex, catch_chance, pokemon_level, pokemon_xp)
         self.evolve_to=evolve_to_str
         self.evolution_lvl=evolution_lvl
 
@@ -146,6 +151,7 @@ class EvolvablePokemon(Pokemon):
         self.type1      = evolved_pokemon.type1
         self.type2      = evolved_pokemon.type2
         self.move       = evolved_pokemon.move
+        self.move2      = evolved_pokemon.move2
         self.weight     = evolved_pokemon.weight
         self.height     = evolved_pokemon.height
 
@@ -163,8 +169,6 @@ def generate_pokemon(player_lvl):
 
     while (pokemon.pokemon_level > (player_lvl + 10) or pokemon.pokemon_level == 0):
 
-        # id needs to be regenerated each time in loop.
-        #If generated outside, can lock program by producing pokemon who has a level range that will never match criteria
         id=random.randint(1,151)
 
         if (id in pokemon_entries.keys()):
@@ -173,11 +177,11 @@ def generate_pokemon(player_lvl):
             type1=pokemon_entries[id]["type1"]
             type2=pokemon_entries[id]["type2"]
             move=pokemon_entries[id]["move"]
+            move2=pokemon_entries[id]["move2"]
             initial_lvl=pokemon_entries[id]["initial_lvl"]
             gen_lvl_cap=pokemon_entries[id]["gen_lvl_cap"]
             catch_chance=pokemon_entries[id]["catch_chance"]
             evolution_lvl=pokemon_entries[id]["evolve_lvl"]
-            #define additional move types
         
             gen_level  = random.randint(int(initial_lvl),int(gen_lvl_cap))
             cp         = random.randint(10*gen_level,10*(gen_level*2))
@@ -186,15 +190,14 @@ def generate_pokemon(player_lvl):
             height     = round(pokemon_entries[id]["height"]*(1+0.5*random.uniform(-1,1)),2)
             sex        = random.choice(["M","F"])
             pokemon_xp = (gen_level-1)*1000
-            #by generating at experience, leveling up right away, needs to be updated
 
             evolvable=pokemon_entries[id]["evolve_to"]!="None"
 
             if(evolvable):
                 evolve_to=pokemon_entries[id]["evolve_to"]
-                pokemon=EvolvablePokemon(id, name, hp, cp, type1, type2, move, weight, height, sex, catch_chance, gen_level, pokemon_xp, evolve_to, evolution_lvl)
+                pokemon=EvolvablePokemon(id, name, hp, cp, type1, type2, move, move2, weight, height, sex, catch_chance, gen_level, pokemon_xp, evolve_to, evolution_lvl)
             else:
-                pokemon=Pokemon(id,name,hp,cp,type1,type2,move,weight,height,sex,catch_chance, gen_level, pokemon_xp)
+                pokemon=Pokemon(id,name,hp,cp,type1,type2,move,move2,weight,height,sex,catch_chance, gen_level, pokemon_xp)
 
         else:
             print("Pokemon does not exist in Pokedex. A default Pokemon will be created.")
@@ -204,7 +207,7 @@ def generate_pokemon(player_lvl):
 
 
 def generate_pokemon_by_name(name_str):
-    #only used for evolutions or generation from preset list (ie creation of new user)
+    """only used for evolutions or generation from preset list (ie creation of new user)"""
 
     pokemon=Pokemon()
 
@@ -215,11 +218,11 @@ def generate_pokemon_by_name(name_str):
             type1=pokemon_entries[id]["type1"]
             type2=pokemon_entries[id]["type2"]
             move=pokemon_entries[id]["move"]
+            move2=pokemon_entries[id]["move2"]
             initial_lvl=pokemon_entries[id]["initial_lvl"]
             gen_lvl_cap=pokemon_entries[id]["gen_lvl_cap"]
             catch_chance=pokemon_entries[id]["catch_chance"]
             evolution_lvl=pokemon_entries[id]["evolve_lvl"]
-            #define additional move types
 
             gen_level  = random.randint(int(initial_lvl),int(gen_lvl_cap))
             cp         = random.randint(10*gen_level,10*(gen_level*2))
@@ -228,53 +231,29 @@ def generate_pokemon_by_name(name_str):
             height     = round(pokemon_entries[id]["height"]*(1+0.5*random.uniform(-1,1)),2)
             sex        = random.choice(["M","F"])
             pokemon_xp = (gen_level-1)*1000
-            #by generating at experience, leveling up right away, needs to be updated
 
             evolvable=pokemon_entries[id]["evolve_to"]!="None"
 
             if(evolvable):
                 evolve_to=pokemon_entries[id]["evolve_to"]
-                pokemon=EvolvablePokemon(id, name, hp, cp, type1, type2, move, weight, height, sex, catch_chance, gen_level, pokemon_xp, evolve_to, evolution_lvl)
+                pokemon=EvolvablePokemon(id, name, hp, cp, type1, type2, move, move2, weight, height, sex, catch_chance, gen_level, pokemon_xp, evolve_to, evolution_lvl)
             else:
-                pokemon=Pokemon(id,name,hp,cp,type1,type2,move,weight,height,sex,catch_chance, gen_level, pokemon_xp)
+                pokemon=Pokemon(id,name,hp,cp,type1,type2,move,move2,weight,height,sex,catch_chance, gen_level, pokemon_xp)
 
     return pokemon
 
-"""
-def generate_pokemon_by_name_select(name_str,player_lvl):
+def battle_type_eval(move_type, target_type1, target_type2):
 
-#Will need while loop readded. Potential usage if given player ability to provide a name from a list and get a pokemon but should be able to use standard function.
+    check1 = Type(move_type,target_type1)
+    check2 = Type(move_type,target_type2)
 
-    pokemon=Pokemon()
+    multiplier = check1.damage_multiplier * check2.damage_multiplier
 
-    for id,pokemon_gen in pokemon_entries.items():
-        if (pokemon_gen["name"].lower()==name_str.lower()):
-
-            name=pokemon_entries[id]["name"]
-            type1=pokemon_entries[id]["type1"]
-            type2=pokemon_entries[id]["type2"]
-            move=pokemon_entries[id]["move"]
-            initial_lvl=pokemon_entries[id]["initial_lvl"]
-            gen_lvl_cap=pokemon_entries[id]["gen_lvl_cap"]
-            catch_chance=pokemon_entries[id]["catch_chance"]
-            evolution_lvl=pokemon_entries[id]["evolve_lvl"]
-            #define additional move types
-
-            gen_level  = random.randint(int(initial_lvl),int(gen_lvl_cap))
-            cp         = random.randint(10*gen_level,10*(gen_level*2))
-            hp         = random.randint(cp//10, 1.5*cp//10)
-            weight     = round(pokemon_entries[id]["weight"]*(1+0.5*random.uniform(-1,1)),2)
-            height     = round(pokemon_entries[id]["height"]*(1+0.5*random.uniform(-1,1)),2)
-            sex        = random.choice(["M","F"])
-            pokemon_xp = (gen_level)*1000
-
-            evolvable=pokemon_entries[id]["evolve_to"]!="None"
-
-            if(evolvable):
-                evolve_to=pokemon_entries[id]["evolve_to"]
-                pokemon=EvolvablePokemon(id, name, hp, cp, type1, type2, move, weight, height, sex, catch_chance, gen_level, pokemon_xp, evolve_to, evolution_lvl)
-            else:
-                pokemon=Pokemon(id,name,hp,cp,type1,type2,move,weight,height,sex,catch_chance, gen_level, pokemon_xp)
-
-    return pokemon
-"""
+    if multiplier == 0:
+        print("It has no effect!")
+    elif multiplier < 1:
+        print("It's not very effective")
+    elif multiplier >= 2:
+        print("It's super effective!")
+        
+    return multiplier
